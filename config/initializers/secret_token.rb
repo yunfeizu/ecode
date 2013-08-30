9a0c1f13689608d2d6baaca8cf573fb7d61f2f32
Yunfei Zu <zuyunfei@gmail.com>
2013-08-30 13:04:09 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ecode::Application.config.secret_key_base = '7a0bedf40ec0cff27aeb408d00168ba97a19d55d1d6c597eba805c420675edc172b251cb30a68ba6dcaffaddb3136447eaf6a7762098bb1b9c04cb116198ef5a'
